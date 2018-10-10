c4179d8f6723da5ee69d4a1c79bbc3feb7c2f56b
Enanyy <742216502@qq.com>
2018-10-10 19:09:34 +0800

    protobuf

--- Assets/Scripts/LuaGame.cs
+++ Assets/Scripts/LuaGame.cs
@@ -2,15 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LuaGame : MonoBehaviour {
+using SLua;
+public partial class LuaGame : MonoBehaviour
+{
+    LuaSvr l;
+    int progress = 0;
+    // Use this for initialization
+    void Start()
+    {
+        Application.logMessageReceived += OnLog;
+        l = new LuaSvr();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        LuaSvr.mainState.loaderDelegate = OnLoad;
+
+        l.init(OnTick, OnComplete, LuaSvrFlag.LSF_BASIC | LuaSvrFlag.LSF_EXTLIB);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnLog(string cond, string trace, LogType lt)
+    {
+        Debug.Log(trace);
+    }
+
+    void OnTick(int p)
+    {
+        progress = p;
+    }
+
+    void OnComplete()
+    {
+        l.start("main");
+    }
+
+    void OnGUI()
+    {
+        if (progress != 100)
+            GUI.Label(new Rect(0, 0, 100, 50), string.Format("Loading {0}%", progress));
+    }
+
+    byte[] OnLoad(string fn, ref string absoluteFn)
+    {
+        string path = string.Format("Assets/Scripts/Lua/{0}.txt", fn.Replace('.','/'));
+        TextAsset asset=  UnityEditor.AssetDatabase.LoadAssetAtPath<TextAsset>(path);
+
+        if (asset!=null)
+        {
+            return asset.bytes;
+        }
+
+        return null;
+    }
+
 }
