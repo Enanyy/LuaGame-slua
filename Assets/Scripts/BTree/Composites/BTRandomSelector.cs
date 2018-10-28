266e6bc0062ad46b1482f18bcbfccf7a4359b5d2
Enanyy <742216502@qq.com>
2018-10-28 20:01:39 +0800

    skill

---
+++ Assets/Scripts/BTree/Composites/BTRandomSelector.cs
@@ -0,0 +1,31 @@
+﻿
+namespace BTree
+{
+    public class BTRandomSelector: BTPrioritySelector
+    {
+        public BTRandomSelector()
+            : base()
+        {
+        }
+        public BTRandomSelector(BTNode _parentNode, BTPrecondition _precondition = null)
+            : base(_parentNode, _precondition)
+        {
+
+        }
+
+        protected override bool OnEvaluate(BTInput _input)
+        {
+            mCurrentSelectIndex = UnityEngine.Random.Range(0, childCount);
+            if(CheckIndex(mCurrentSelectIndex))
+            {
+                var node = children[mCurrentSelectIndex];
+                if (node.Evaluate(_input))
+                {
+                    return true;
+                }
+            }
+
+            return base.OnEvaluate(_input);
+        }
+    }
+}
