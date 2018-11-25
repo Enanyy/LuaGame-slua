9996ac5dd31869b15eb65c5faadccfa0bb835ce2
Enanyy <742216502@qq.com>
2018-11-25 15:12:05 +0800

    Asset

--- Assets/Scripts/Network/NetPacket.cs
+++ Assets/Scripts/Network/NetPacket.cs
@@ -15,7 +15,8 @@ namespace Network
         }
 
         private byte[] mData;
-        private int mPosition;
+        public int writePosition { get; private set; }
+        public int readPosition { get; private set; }
         public byte[] data { get { return mData; } }
 
         public int Length
@@ -23,18 +24,6 @@ namespace Network
             get
             {
                 return mData.Length;
-            }
-        }
-
-        public int Position
-        {
-            get
-            {
-                return mPosition;
-            }
-            set
-            {
-                mPosition = value;
             }
         }
 
@@ -46,20 +35,32 @@ namespace Network
         public void SetData(byte[] data)
         {
             mData = data;
-            mPosition = 0;
+            writePosition = 0;
+            readPosition = 0;
         }
         public int ReadInt()
         {
-            int oldPos = mPosition;
-            mPosition += 4;
+            int oldPos = readPosition;
+            readPosition += 4;
             return BitConverter.ToInt32(mData, oldPos);
+        }    
+        public void WriteInt(int v)
+        {
+            ReAlloc(ref mData, writePosition, 4);
+            Array.Copy(BitConverter.GetBytes(v), 0, mData, writePosition, 4);
+            writePosition += 4;
         }
-
         public uint ReadUInt()
         {
-            int oldPos = mPosition;
-            mPosition += 4;
+            int oldPos = readPosition;
+            readPosition += 4;
             return BitConverter.ToUInt32(mData, oldPos);
+        }
+        public void WriteUInt(uint v)
+        {
+            ReAlloc(ref mData, writePosition, 4);
+            Array.Copy(BitConverter.GetBytes(v), 0, mData, writePosition, 4);
+            writePosition += 4;
         }
 
         public short ReadShort()
@@ -67,81 +68,91 @@ namespace Network
             return ReadInt16();
         }
 
+        public void WriteShort(short v)
+        {
+            ReAlloc(ref mData, writePosition, 2);
+            Array.Copy(BitConverter.GetBytes(v), 0, mData, writePosition, 2);
+            writePosition += 2;
+        }
+
         public ushort ReadUShort()
         {
             return ReadUInt16();
         }
 
+        public void WriteUShort(ushort v)
+        {
+            ReAlloc(ref mData, writePosition, 2);
+            Array.Copy(BitConverter.GetBytes(v), 0, mData, writePosition, 2);
+            writePosition += 2;
+        }
+
+       
         public short ReadInt16()
         {
-            int oldPos = mPosition;
-            mPosition += 2;
+            int oldPos = readPosition;
+            readPosition += 2;
             return BitConverter.ToInt16(mData, oldPos);
         }
 
         public ushort ReadUInt16()
         {
-            int oldPos = mPosition;
-            mPosition += 2;
+            int oldPos = readPosition;
+            readPosition += 2;
             return BitConverter.ToUInt16(mData, oldPos);
         }
 
         public Int64 ReadInt64()
         {
-            int oldPos = mPosition;
-            mPosition += 8;
+            int oldPos = readPosition;
+            readPosition += 8;
             return BitConverter.ToInt64(mData, oldPos);
-        }
-
-        public UInt64 ReadUInt64()
-        {
-            int oldPos = mPosition;
-            mPosition += 8;
-            return BitConverter.ToUInt64(mData, oldPos);
-        }
-
-        public void WriteInt(int v)
-        {
-            ReAlloc(ref mData, mPosition, 4);
-            Array.Copy(BitConverter.GetBytes(v), 0, mData, mPosition, 4);
-            mPosition += 4;
-        }
-        public void WriteUInt(uint v)
-        {
-            ReAlloc(ref mData, mPosition, 4);
-            Array.Copy(BitConverter.GetBytes(v), 0, mData, mPosition, 4);
-            mPosition += 4;
-        }
-        public void WriteUShort(ushort v)
-        {
-            ReAlloc(ref mData, mPosition, 2);
-            Array.Copy(BitConverter.GetBytes(v), 0, mData, mPosition, 2);
-            mPosition += 2;
-        }
-
-        public void WriteShort(short v)
-        {
-            ReAlloc(ref mData, mPosition, 2);
-            Array.Copy(BitConverter.GetBytes(v), 0, mData, mPosition, 2);
-            mPosition += 2;
         }
         public void WriteInt64(Int64 v)
         {
-            ReAlloc(ref mData, mPosition, 8);
-            Array.Copy(BitConverter.GetBytes(v), 0, mData, mPosition, 8);
-            mPosition += 8;
+            ReAlloc(ref mData, writePosition, 8);
+            Array.Copy(BitConverter.GetBytes(v), 0, mData, writePosition, 8);
+            writePosition += 8;
         }
-
+        public UInt64 ReadUInt64()
+        {
+            int oldPos = readPosition;
+            readPosition += 8;
+            return BitConverter.ToUInt64(mData, oldPos);
+        }
         public void WriteUInt64(UInt64 v)
         {
-            ReAlloc(ref mData, mPosition, 8);
-            Array.Copy(BitConverter.GetBytes(v), 0, mData, mPosition, 8);
-            mPosition += 8;
+            ReAlloc(ref mData, writePosition, 8);
+            Array.Copy(BitConverter.GetBytes(v), 0, mData, writePosition, 8);
+            writePosition += 8;
+        }
+
+        public byte[] ReadBytes()
+        {
+            int len = ReadInt();
+            int oldPos = readPosition;
+            readPosition += len;
+            byte[] bytes = new byte[len];
+            for (int i = 0; i < len; ++i)
+            {
+                bytes[i] = mData[oldPos + i];
+            }
+            return bytes;
+        }
+
+        public void WriteBytes(byte[] v)
+        {
+            int len = v.Length;
+            WriteInt(len);
+            ReAlloc(ref mData, writePosition, len);
+            v.CopyTo(mData, writePosition);
+            writePosition += len;
         }
 
         public void Clear()
         {
-            mPosition = 0;
+            writePosition = 0;
+            readPosition = 0;
         }
     }
 }
